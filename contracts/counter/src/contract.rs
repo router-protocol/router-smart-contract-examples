be8d88588a6fd303bc0f3ce58707869e645d59e4
kunalroute <kunal@routerprotocol.com>
2023-04-05 15:29:43 +0400

    add: ibc callbacks

--- contracts/counter/src/contract.rs
+++ contracts/counter/src/contract.rs
@@ -2,12 +2,18 @@ use crate::msg::{ExecuteMsg, InstantiateMsg, MigrateMsg, QueryMsg};
 use crate::state::COUNTER;
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::{entry_point, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
-use cosmwasm_std::{to_binary, StdError};
+use cosmwasm_std::{to_binary, StdError, Empty, Event,
+    IbcChannelOpenMsg, IbcChannelOpenResponse, Ibc3ChannelOpenResponse,
+    IbcChannelCloseMsg,
+    IbcBasicResponse,
+    IbcChannelConnectMsg, IbcOrder, IbcPacketAckMsg, IbcPacketReceiveMsg, IbcPacketTimeoutMsg, IbcReceiveResponse,
+    WasmMsg, SubMsg};
 use cw2::{get_contract_version, set_contract_version};
 
 // version info for migration info
 pub const CONTRACT_NAME: &str = "counter";
 pub const CONTRACT_VERSION: &str = "0.1.0";
+pub const INIT_CALLBACK_ID: u64 = 7890;
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
@@ -77,3 +83,113 @@ pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
 fn query_counter(deps: Deps) -> StdResult<u32> {
     return Ok(COUNTER.load(deps.storage)?);
 }
+
+
+#[entry_point]
+/// enforces ordering and versioing constraints
+pub fn ibc_channel_open(
+    _deps: DepsMut,
+    _env: Env,
+    msg: IbcChannelOpenMsg,
+) -> StdResult<IbcChannelOpenResponse> {
+    
+    Ok(Some(Ibc3ChannelOpenResponse {
+        version: CONTRACT_VERSION.to_string(),
+    }))
+}
+
+#[entry_point]
+/// once it's established, we create the reflect contract
+pub fn ibc_channel_connect(
+    deps: DepsMut,
+    _env: Env,
+    msg: IbcChannelConnectMsg,
+) -> StdResult<IbcBasicResponse> {
+
+    let msg = WasmMsg::Instantiate {
+        admin: None,
+        code_id: 0,
+        msg: b"{}".into(),
+        funds: vec![],
+        label: format!("ibc-reflect-{}", 1),
+    };
+    let subMsg = SubMsg::reply_on_success(msg, INIT_CALLBACK_ID);
+
+    Ok(IbcBasicResponse::new()
+        .add_submessage(subMsg)
+        .add_attribute("action", "ibc_connect")
+        .add_attribute("channel_id", "chan_id")
+        .add_event(Event::new("ibc").add_attribute("channel", "connect")))
+}
+
+#[entry_point]
+/// On closed channel, we take all tokens from reflect contract to this contract.
+/// We also delete the channel entry from accounts.
+pub fn ibc_channel_close(
+    deps: DepsMut,
+    env: Env,
+    msg: IbcChannelCloseMsg,
+) -> StdResult<IbcBasicResponse> {
+
+    let messages: Vec<SubMsg<Empty>> = vec![];
+
+    Ok(IbcBasicResponse::new()
+        .add_submessages(messages)
+        .add_attribute("action", "ibc_close")
+        .add_attribute("channel_id", "channel_id")
+        .add_attribute("steal_funds", "steal_funds".to_string()))
+}
+
+#[entry_point]
+/// never should be called as we do not send packets
+pub fn ibc_packet_ack(
+    _deps: DepsMut,
+    _env: Env,
+    _msg: IbcPacketAckMsg,
+) -> StdResult<IbcBasicResponse> {
+    Ok(IbcBasicResponse::new().add_attribute("action", "ibc_packet_ack"))
+}
+
+#[entry_point]
+/// never should be called as we do not send packets
+pub fn ibc_packet_timeout(
+    _deps: DepsMut,
+    _env: Env,
+    _msg: IbcPacketTimeoutMsg,
+) -> StdResult<IbcBasicResponse> {
+    Ok(IbcBasicResponse::new().add_attribute("action", "ibc_packet_timeout"))
+}
+
+// #[entry_point]
+// /// we look for a the proper reflect contract to relay to and send the message
+// /// We cannot return any meaningful response value as we do not know the response value
+// /// of execution. We just return ok if we dispatched, error if we failed to dispatch
+// pub fn ibc_packet_receive(
+//     deps: DepsMut,
+//     _env: Env,
+//     msg: IbcPacketReceiveMsg,
+// ) -> StdResult<IbcReceiveResponse> {
+//     // put this in a closure so we can convert all error responses into acknowledgements
+//     // (|| {
+//     //     let packet = msg.packet;
+//     //     // which local channel did this packet come on
+//     //     let caller = packet.dest.channel_id;
+//     //     let msg: PacketMsg = from_slice(&packet.data)?;
+//     //     // match msg {
+//     //     //     PacketMsg::Dispatch { msgs } => receive_dispatch(deps, caller, msgs),
+//     //     // }
+//     // })()
+//     // .or_else(|e| {
+//     //     // we try to capture all app-level errors and convert them into
+//     //     // acknowledgement packets that contain an error code.
+//     //     let acknowledgement = encode_ibc_error(format!("invalid packet: {}", e));
+//     //     Ok(IbcReceiveResponse::new()
+//     //         .set_ack(acknowledgement)
+//     //         .add_event(Event::new("ibc").add_attribute("packet", "receive")))
+//     // })
+
+//     //let acknowledgement = encode_ibc_error(format!("invalid packet: {}", e));
+//     Ok(IbcReceiveResponse::new()
+//         //.set_ack("acknowledgement")
+//         .add_event(Event::new("ibc").add_attribute("packet", "receive")))
+// }
